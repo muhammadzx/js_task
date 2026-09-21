62ea1ecf1b551655dcdd49125c8bb64bb09c252c
muhammadzx <mohammad.alshawabkeh752@gmail.com>
2026-09-21 15:13:22 +0300

    Initial commit

---
+++ Mon_21_9_2026/js_task.js
@@ -0,0 +1,40 @@
+console.log(typeof(100))//output Number
+console.log(typeof(73.9))// Number
+console.log(typeof(NaN))//Number
+console.log(typeof("Water"))//strinfg
+console.log(typeof(false))//boolean
+console.log(typeof(9 != 11))//boolean
+console.log("Orang" + "e")//Orange
+console.log("Orange" - "s")//oranges
+console.log("4" + "8")//48
+console.log("4" - "8")//-4
+console.log("name" + 3)//name3
+console.log("name" - 3)//NaN
+console.log(82 * "word")//NaN
+console.log("hello" + 1)//hello1
+console.log(1+"hello")//1hello
+console.log(1 + true)//2
+console.log("hello" + true)//hellotrue
+console.log(typeof (Infinity))//number
+console.log(1 == '1')//true
+console.log(1 === '1')//false
+
+ //output Number
+
+
+
+
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
