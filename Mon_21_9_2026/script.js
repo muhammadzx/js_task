c1b158db7376779472dd93c202d8ace925664c5d
muhammadzx <mohammad.alshawabkeh752@gmail.com>
2026-09-21 22:07:15 +0300

    Update index.html

---
+++ Mon_21_9_2026/script.js
@@ -0,0 +1,16 @@
+'use strict'
+function test_function(){
+    var v=10;
+    console.log(v);
+}
+test_function()
+// z=3;
+// console.log(z)
+let name = "Mohammad Alshawabkeh";
+let number =1;
+let bool = true;
+let nullv=null;
+let unde=undefined;
+let opj_val={name:"ahmad",age:22}
+console.log(typeof(name))
+console.log(typeof(opj_val))
